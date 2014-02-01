935ff39f0d82f3064e5c99163f3098a7c79ad254
Zanele Munyikwa <ztm3@duke.edu>
2014-02-01 18:46:34 -0500

    environment parser updated minus walls

---
+++ src/ObjectsParser.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+
+public class ObjectsParser {
+public static void main(String args[]) {
+try {
+	
+	
+	
+} catch (Exception ex) {
+	
+	ex.printStackTrace();
+	
+}		
+}
+
+
+}
